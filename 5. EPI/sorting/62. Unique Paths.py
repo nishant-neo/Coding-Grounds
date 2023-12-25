7904e93a365b556a66b74dbc40f14661d540b58f
Nishant Oli <nishantoli95@gmail.com>
2023-12-25 12:44:33 +0530

    Update 62. Unique Paths.py

--- 5. EPI/sorting/62. Unique Paths.py
+++ 5. EPI/sorting/62. Unique Paths.py
@@ -37,17 +37,17 @@ Constraints:
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
 
-        ways = [[0 for i in range(n+1)] for _ in range(m+1)]
+         ways = [[0 for i in range(n)] for _ in range(m)]
 
-        for i in range(m+1):
+        for i in range(m):
             ways[i][0] = 1
 
-        for i in range(n+1):
+        for i in range(n):
             ways[0][i] = 1
 
 
-        for i in range(1,m+1):
-            for j in range(1,n+1):
+        for i in range(1,m):
+            for j in range(1,n):
                 ways[i][j] = ways[i-1][j] + ways[i][j-1]
 
 
