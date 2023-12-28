5ccc481fe5cc0cc9349dc6ee91d21efe5d5b2623
Nishant Oli <nishantoli95@gmail.com>
2023-12-28 09:58:25 +0530

    Update 1727. Largest Submatrix With Rearrangements.py

--- 5. EPI/sorting/1727. Largest Submatrix With Rearrangements.py
+++ 5. EPI/sorting/1727. Largest Submatrix With Rearrangements.py
@@ -69,10 +69,40 @@ class Solution:
                 largest_matrix = max(largest_matrix, matrix_size)
                 matrix_size  = 0
 
-            # Sorting
+            # Sorting, also slow
             # sorted_row = sorted(subsequence[row], reverse=True)
             # for i in range(C):
             #     largest_matrix = max(largest_matrix, sorted_row[i] * (i + 1))
+        
+        return largest_matrix
+
+
+        # O(R*C) Solution
+
+        m = len(matrix)
+        n = len(matrix[0])
+        prev_heights = []
+        ans = 0
+
+        for row in range(m):
+            heights = []
+            seen = [False] * n
+            
+            for height, col in prev_heights:
+                if matrix[row][col] == 1:
+                    heights.append((height + 1, col))
+                    seen[col] = True
+
+            for col in range(n):
+                if seen[col] == False and matrix[row][col] == 1:
+                    heights.append((1, col))
+            
+            for i in range(len(heights)):
+                ans = max(ans, heights[i][0] * (i + 1))
+                
+            prev_heights = heights
+
+        return ans
                 
 
-        return largest_matrix
+        
