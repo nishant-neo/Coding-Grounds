2f16f8f3aaa88c2a9c3bfe084c1eac85eca909b9
Nishant Oli <nishantoli95@gmail.com>
2023-12-28 12:27:57 +0530

    Create 366. Find Leaves of Binary Tree.py

---
+++ 5. EPI/sorting/366. Find Leaves of Binary Tree.py
@@ -0,0 +1,64 @@
+"""
+366. Find Leaves of Binary Tree
+Solved
+Medium
+
+Topics
+
+Companies
+Given the root of a binary tree, collect a tree's nodes as if you were doing this:
+
+Collect all the leaf nodes.
+Remove all the leaf nodes.
+Repeat until the tree is empty.
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4,5]
+Output: [[4,5,3],[2],[1]]
+Explanation:
+[[3,5,4],[2],[1]] and [[3,4,5],[2],[1]] are also considered correct answers since per each level it does not matter the order on which elements are returned.
+Example 2:
+
+Input: root = [1]
+Output: [[1]]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 100].
+-100 <= Node.val <= 100
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findLeaves(self, root: Optional[TreeNode]) -> List[List[int]]:
+        result = []
+        leaves = []
+        def dfs(node):
+            if node is None:
+                return None
+
+            if node.left is None and node.right is None:
+                leaves.append(node.val)
+                node = None
+                return node
+            if node.left:
+                node.left = dfs(node.left)
+            if node.right:
+                node.right = dfs(node.right)
+            return node
+
+        while(root):
+            leaves =[]
+            root = dfs(root)
+            result.append(leaves)
+
+        return result
